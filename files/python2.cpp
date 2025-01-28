69997128a179e90430ad1454de7a4b6c4d408c23
AKSHAI SUNDARESHA <sundaresh832@gmail.com>
2025-01-28 20:23:14 +0530

    add python file

---
+++ files/python2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>  // Include the input-output stream library
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;  // Output the message to the console
+    return 0;  // Indicate that the program ended successfully
+}
+
